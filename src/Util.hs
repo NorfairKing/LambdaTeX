e19a34e1ed7813c154c6b9f9a60891e155c08004
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2016-01-10 04:12:35 +0100

    something rather useful

--- src/Util.hs
+++ src/Util.hs
@@ -2,51 +2,78 @@
 module Util where
 
 import           Data.Char        (toLower)
+import           Data.List        (find)
 import           Data.Monoid      ((<>))
 
 import           System.Directory (createDirectoryIfMissing)
 import           System.FilePath  (takeDirectory, (<.>), (</>))
 
-import           Data.Text        (Text)
-import qualified Data.Text        as T
-import qualified Data.Text.IO     as T
-
-type ModuleName = Text
+type ModuleName = String
 data Module = Module ModuleName -- ^ Name
                      [Import] -- ^ Imports
                      [Dec] -- ^ Declarations
+  deriving (Show, Eq)
 
+makeModule :: ModuleName -> [Import] -> [Dec] -> Module
+makeModule name is ds = Module name is ds'
+  where ds' = makeNamesUnique ds
 
-data Import = Import Text
-type CommentContent = Text
-type TypeSignature = Text
-type Implementation = Text
+makeNamesUnique :: [Dec] -> [Dec]
+makeNamesUnique = foldl addDec []
+  where
+    addDec :: [Dec] -> Dec -> [Dec]
+    addDec ds c@(Comment _) = ds ++ [c]
+    addDec ds f@(Function name _ _)
+        = case find (\(Function n _ _) -> n == name) (filter isFunction ds) of
+            Nothing -> ds ++ [f]
+            Just f -> addDec ds (alterFunction f)
 
-data Dec = Function TypeSignature Implementation
+    isFunction (Function _ _ _) = True
+    isFunction _ = False
+
+    alterFunction (Function name sig impl) = function (alterName name) sig impl
+    alterName = (++ "_")
+
+data Import = Import String
+  deriving (Show, Eq)
+
+type CommentContent = String
+type TypeSignature = String
+type Implementation = String
+type FunctionName = String
+
+data Dec = Function FunctionName TypeSignature Implementation
          | Comment CommentContent
+  deriving (Show, Eq)
 
+function :: FunctionName -> TypeSignature -> Implementation -> Dec
+function name sig impl = Function name' sig impl
+  where name' = cleanName name
+
+cleanName :: String -> String
+cleanName (f:rest) = toLower f : rest
 
 class RenderSrc a where
-    render :: a -> Text
+    render :: a -> String
 
 instance RenderSrc Module where
     render (Module name is ds) =
-        T.unlines $ ("module " <> name <> " where") : "\n" : map render is <> ["\n"] <> map render ds
+        unlines $ ("module " <> name <> " where") : "\n" : map render is <> ["\n"] <> map render ds
 
 instance RenderSrc Dec where
-    render (Function ts i) = T.unlines [ts, i]
-    render (Comment s) = T.unwords ["--", s]
+    render (Function name ts i) = unlines [unwords [name, "::", ts], unwords [name, "=", i]]
+    render (Comment s) = unwords ["--", s]
 
 instance RenderSrc Import where
     render (Import i) = "import " <> i
 
 main :: IO ()
-main = generateModule myModule
+main = putStrLn (render myModule) >> generateModule myModule
 
 nameToFile :: ModuleName -> String
-nameToFile name = map (\c -> if c == '.' then '/' else c) (T.unpack name) <.> "hs"
+nameToFile name = map (\c -> if c == '.' then '/' else c) name <.> "hs"
 
-moduleName :: Module -> Text
+moduleName :: Module -> String
 moduleName (Module n _ _) = n
 
 generateModule :: Module -> IO ()
@@ -56,54 +83,55 @@ generateModule mod = do
     let file = src </> filename
     createDirectoryIfMissing True $ takeDirectory file
     let contents = render mod
-    T.writeFile file contents
+    writeFile file contents
 
 
-lit :: Text -> Text
+lit :: String -> String
 lit l = "\"" <> l <> "\""
 
-func :: Text -> Text
-func = T.map toLower
+func :: String -> String
+func = map toLower
 
-typeDec :: Text -> Int -> Text
-typeDec name n = T.unwords $ [func name, "::", "Monad m", "=>"] ++ concat (replicate n [λtex, "->"]) ++ [λtex]
+typeDec :: Int -> String
+typeDec n = unwords $ ["Monad m", "=>"] ++ concat (replicate n [λtex, "->"]) ++ [λtex]
   where λtex = "ΛTeXT m ()"
 
-makeCommS :: Text -> [Dec]
+
+makeCommS :: String -> [Dec]
 makeCommS name =
     [
       Comment $ "| The @\\\\" <> name <> "@ command"
     , Comment ""
     , Comment $ ">>> " <> name
-    , Comment $ T.unwords $ [">>>", name]
+    , Comment $ unwords $ [">>>", name]
     , Comment $ "\\" <> name
-    , Function (typeDec name 0) dec
+    , function name (typeDec 0) dec
     ]
-  where dec = T.unwords [func name, "=", "commS", lit name]
+  where dec = unwords ["commS", lit name]
 
-makeComm0 :: Text -> [Dec]
+makeComm0 :: String -> [Dec]
 makeComm0 name =
     [
       Comment $ "| The @\\\\" <> name <> "@ command"
     , Comment ""
-    , Comment $ T.unwords $ [">>>", name]
+    , Comment $ unwords $ [">>>", name]
     , Comment $ "\\" <> name <> "{}"
-    , Function (typeDec name 0) dec
+    , function name (typeDec 0) dec
     ]
-  where dec = T.unwords [func name, "=", "comm0", lit name]
+  where dec = unwords ["comm0", lit name]
 
-makeComm1 :: Text -> [Dec]
+makeComm1 :: String -> [Dec]
 makeComm1 name = [
       Comment $ "| The @\\\\" <> name <> "@ command"
     , Comment ""
-    , Comment $ T.unwords $ [">>>", name, "\"a\""]
+    , Comment $ unwords $ [">>>", name, "\"a\""]
     , Comment $ "\\" <> name <> "{a}"
-    , Function (typeDec name 1) dec
+    , function name (typeDec 1) dec
     ]
-  where dec = T.unwords [func name, "=", "comm1", lit name]
+  where dec = unwords ["comm1", lit name]
 
 myModule :: Module
-myModule = Module "Text.LaTeX.Commands.Base"
+myModule = makeModule "Text.LaTeX.Commands.Base"
     [
       Import "Text.LaTeX.LambdaTeX"
     , Import "Text.LaTeX.Base.Class (comm0, commS, comm1)"
@@ -113,7 +141,7 @@ myModule = Module "Text.LaTeX.Commands.Base"
         ++ map makeComm0 comm0s
         ++ map makeComm1 comm1s
 
-commSs :: [Text]
+commSs :: [String]
 commSs =
     [
       "hyp"
@@ -123,7 +151,7 @@ commSs =
     , "hline"
     ]
 
-comm0s :: [Text]
+comm0s :: [String]
 comm0s =
     [
       "maketitle"
@@ -152,7 +180,7 @@ comm0s =
     , "bigskip"
     ]
 
-comm1s :: [Text]
+comm1s :: [String]
 comm1s =
     [
       "title"
@@ -166,7 +194,8 @@ comm1s =
     , "subsubsection"
     , "paragraph"
     , "subparagraph"
-    , "cite"
+    , "caption"
+    -- , "cite"
     , "pagenumbering"
     , "markright"
     , "hyphenation"
@@ -184,9 +213,19 @@ comm1s =
     , "textsl"
     , "textsc"
     , "textnormal"
+    , "tiny"
+    , "scriptsize"
+    , "footnotesize"
+    , "small"
+    , "normalsize"
+    , "large"
+    , "Large"
+    , "LARGE"
+    , "huge"
+    , "Huge"
     ]
 
-envs :: [Text]
+envs :: [String]
 envs =
     [
       "enumerate"
